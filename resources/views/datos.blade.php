81a7c827da54042aff1b55ed3f43d9ceab86a9c5
Osvaldo Rene Rojo Roa <osvaldo.rojo@zapotlanejo.tecmm.edu.mx>
2022-09-08 12:43:28 -0500

    Primera carga

---
+++ resources/views/datos.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+@section('content')
+<div class="container-fluid bg-blue-100">
+    <div class="mt-10 max-w-2xl mx-auto text-center space-y-3">
+        <h1 class="text-4xl font-extrabold text-indigo-600 mb-10" >
+            Ejemplo de FRAMEWORK con LARAVEL
+        </h1>       
+    </div>
+    <div class="mt-10 max-w-2xl mx-auto shadow bg-white p-10 text-base">
+          HOLA MUNDO
+    </div>
+</div>
+@endsection
---
+++ resources/views/plantilla/base.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    <!--Inicio del menu-->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Informatica</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Suma</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Multiplicacion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Division</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid bg-blue-100">
+<div class="mt-10 max-w-2xl mx-auto shadow bg-white p-5 text-base">
+    <!--Fin del menu-->
+
+       @yield('content')
+</div>  
+       <!-- Optional JavaScript; choose one of the two! -->
+</div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @include('footer')
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
